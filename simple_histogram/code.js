934d4d61d16094fcc43159e13e4a1b696644f690
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-16 10:21:13 -0800

    Creates axes

--- simple_histogram/code.js
+++ simple_histogram/code.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   let width = 900, height = 300, pad=20, left_pad=100;
   let x = d3.scaleBand().range([left_pad, width-pad]).round(0.1);
-  let y = d3.scaleOrdinal().range([height-pad, pad]);
+  let y = d3.scaleLinear().range([height-pad, pad]);
   let xAxis = d3.axisBottom(x);
   let yAxis = d3.axisLeft(y);
 
   let svg = d3.select("#graph").append('svg').attr("height", height).attr("width", width);
+
+  let data;
+  d3.json("histogram_hours.json", function(err, jsonData){
+    data = d3.keys(jsonData).map((key) => {
+      return {bucket: parseInt(key), N: jsonData[key]};
+    });
+    console.log(data);
+    x.domain(data.map((el) => el.bucket));
+    y.domain([0, d3.max(data, function(d){return d.N;})]);
+
+    svg.append("g").attr("class", "axis").attr("transform", "translate(0, "+(height-pad) + ")").call(xAxis);
+    svg.append("g").attr("class", "axis").attr("transform", "translate("+(left_pad -pad) + ", 0)").call(yAxis);
+  });
 });
